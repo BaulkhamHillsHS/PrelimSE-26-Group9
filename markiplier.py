5c682b73f5e44464f2acbde4f8bd4e00e818719b
foobarFighters1 <isaac.daniel7@education.nsw.gov.au>
2026-06-21 20:26:52 +1000

    fixed previous errors, added a function to watch

--- markiplier.py
+++ markiplier.py
@@ -7,6 +7,7 @@ import os
 from tkinter import filedialog, messagebox
 import csv
 from PIL import Image
+import ast
 #import god as pleasesavethisproject
 
 ## Movies and TV Shows
@@ -38,8 +39,35 @@ movies = {
     "Drive": "adult",
     "Mad Max: Fury Road": "adult"
 }
+anime = {
+    "Naruto": "child",
+    "Pokémon": "child",
+    "Doraemon": "child",
+    "Yo-kai Watch": "child",
+    "Beyblade": "child",
+    "Digimon Adventure": "child",
+    "Cardcaptor Sakura": "child",
+    "Hamtaro": "child",
+    "Chi's Sweet Home": "child",
+    "Anpanman": "child",
+    "Pretty Cure": "child",
+    "Inazuma Eleven": "child",
 
-
+    "Attack on Titan": "adult",
+    "Death Note": "adult",
+    "Tokyo Ghoul": "adult",
+    "Monster": "adult",
+    "Psycho-Pass": "adult",
+    "Parasyte": "adult",
+    "Black Lagoon": "adult",
+    "Hellsing Ultimate": "adult",
+    "Berserk": "adult",
+    "Vinland Saga": "adult",
+    "Chainsaw Man": "adult",
+    "Devilman Crybaby": "adult",
+    "Perfect Blue": "adult"
+}
+movies = movies | anime
 # Themes
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -48,7 +76,9 @@ ctk.set_default_color_theme("blue")
 logged_in = False
 
 profiles_and_ages = {}
-
+watching = ""
+age = ""
+profile = ""
 # Login window
 class LoginWindow(ctk.CTk):
     def __init__(self):
@@ -92,7 +122,7 @@ Please input your username
 and password to log in.""").grid(row = 0, column = 1, padx = 20, pady = 20, sticky = "n")
         self.username_input = ctk.CTkEntry(self.frame_input, width = 160, placeholder_text="Username")
         self.username_input.grid(row = 1, column = 1, padx = 20, pady = 10, sticky = "n")
-        self.password_input = ctk.CTkEntry(self.frame_input, width = 160, placeholder_text="Password")
+        self.password_input = ctk.CTkEntry(self.frame_input, width = 160, placeholder_text="Password", show = "*")
         self.password_input.grid(row = 2, column = 1, padx = 20, pady = 5, sticky = "n")
         self.button = ctk.CTkButton(self.frame_input, width = 160, height = 28, text = "Log in", command=self.button_pressed)
         self.button.grid(row = 4, column = 1, padx = 20, pady = 20, sticky = "n")
@@ -143,7 +173,11 @@ class MainWindow(ctk.CTk):
         self.name.pack()
         self.profile_checker = ctk.CTkComboBox(self.frame_main, values = list(profiles_and_ages.keys()), command = self.age_configure)
         self.profile_checker.pack()
-        self.age = ctk.CTkLabel(self.frame_main, text = f"Age: {profiles_and_ages[self.profile_checker.get()]}")
+        self.profile_checker.set(list(profiles_and_ages.keys())[0])
+        global profile, age
+        profile = self.profile_checker.get()
+        age = profiles_and_ages[profile]
+        self.age = ctk.CTkLabel(self.frame_main, text=f"Age: {age}")
         self.age.pack()
         self.profile_selector = ctk.CTkButton(self.frame_main, width = 100, height = 34, text = "Select profile", command = self.profile_button_pressed)
         self.profile_selector.pack(pady = 20)
@@ -166,16 +200,57 @@ class MainWindow(ctk.CTk):
                 self.movies_box.insert("end", item)
                 
     def age_configure(self, choice):
-        print("Selected:", choice)
+        global age
+        global profile
+        age = profiles_and_ages[choice]
+        profile = choice
         self.age.configure(text=f"Age: {profiles_and_ages[choice]}")
         return choice
     
     def show_value(self, selected_option):
-        print(movies[selected_option])
+        global watching
+        watching = selected_option
+        self.destroy()
+        media = MediaWindow()
+        media.mainloop()
     
     def profile_button_pressed(self):
         print("profile button pressed")
     # should probably be used to like log into specific profiles
+
+class MediaWindow(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Media Player")
+        self.geometry("600x400+500+150")
+        self.grid_rowconfigure(1, weight = 1)
+        self.grid_columnconfigure(2, weight = 0)
+        self.resizable(False, False)
+        self.background_build()
+        ##self.main_screen_build()
+    
+    def background_build(self):
+        self.background_image = ctk.CTkImage(light_image = Image.open("images/ui/ProfileBackground.png"), dark_image = Image.open("images/ui/ProfileBackground.png"), size = (600, 400))
+        self.background_image_label = ctk.CTkLabel(self, text = "", image = self.background_image)
+        self.background_image_label.place(x = 0, y = 0)
+        self.watching = ctk.CTkLabel(self, text = watching)
+        self.watching.pack()
+        self.watch_button = ctk.CTkButton(self, text = "Watch", command = self.watch_check)
+        self.watch_button.pack()
+
+    def watch_check(self):
+        if age.lower() == "child" and movies[watching] == "adult":
+            ##Jonathan can you make like an error label like you did for password above
+            pass
+        else:
+            with open(f"profiles/{profile}.txt", "r", encoding="utf-8") as file:
+                content = file.read()
+            watchhistory = ast.literal_eval(content or "[]")
+            if watching not in watchhistory:
+                watchhistory.append(watching)
+            with open(f"profiles/{profile}.txt", "w", encoding="utf-8") as file:
+                file.write(str(watchhistory))
+
 
 if __name__ == "__main__":
     markiplier = LoginWindow()
